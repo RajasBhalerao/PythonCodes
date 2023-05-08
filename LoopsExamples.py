aa228516dd3575b7d209c43139f6f0931daa79ba
RajasBhalerao <bhalerao.rajas1@gmail.com>
2023-05-08 13:34:57 +0530

    Second Commit

--- LoopsExamples.py
+++ LoopsExamples.py
@@ -51,3 +51,81 @@ for i in numbers:
         break
     elif i % 5 == 0:
         print(i)
+"""Create a pathern design"""
+for i in range(0,5):
+    for j in range(0, i+1):
+        print("X", end=' ')
+    print("\r")
+for i in range(5,0,-1):
+    for j in range(0, i-1):
+        print("X", end=' ')
+    print("\r")
+
+
+"""Cubing number to a given range"""
+
+num = input("Add number:")
+input_number = int(num)
+
+res = 0
+for i in range(1,input_number+1):
+    res = i * i * i
+    print(res)
+
+"""Reversing numbers"""
+
+nums = [1,2,3,4]
+nums.reverse()
+print(nums)
+var1, var2, *remaining = nums
+print(var1,var2, *remaining)
+
+num = 76542
+reverse_number = 0
+print("Given Number ", num)
+while num > 0:
+    reminder = num % 10
+    reverse_number = (reverse_number * 10) + reminder
+    num = num // 10
+print("Revere Number ", reverse_number)
+
+
+
+"""Factorial of a given number"""
+inum = input("Enter number:")
+num = int(inum)
+count = 1
+res = 1
+for i in range(num, 0, -1):
+    res = int(res * count)
+    count = i
+print(res)
+
+
+
+"""Fibonacci Sequence"""
+
+start = 0
+end = 10
+
+
+num1 = 0
+num2 = 1
+for i in range(start,end):
+    print(num1)
+    result = num1 + num2
+
+    num1 = num2
+    num2 = result
+
+"""Prime numbers in a range"""
+start = 25
+end = 50
+
+for num in range(start,end):
+    if num >1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
