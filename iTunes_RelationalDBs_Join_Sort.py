ee0bb95077dbd62e4f183e6b3200300c6c76516b
RajasBhalerao <bhalerao.rajas1@gmail.com>
2023-05-15 00:12:19 +0530

    Second commit

--- iTunes_RelationalDBs_Join_Sort.py
+++ iTunes_RelationalDBs_Join_Sort.py
@@ -107,4 +107,4 @@ for entry in all:
     FROM Track JOIN Genre JOIN Album JOIN Artist 
     ON Track.genre_id = Genre.id and Track.album_id = Album.id 
         AND Album.artist_id = Artist.id
-    ORDER BY Artist.name LIMIT 3;"""
+    ORDER BY Artist.name LIMIT 3; """
